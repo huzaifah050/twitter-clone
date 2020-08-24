9d4335c5971d5db455833bae82f53c37bcd62649
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-08-24 00:31:22 +0000

    feat: auth is loaded

---
+++ src/components/AuthIsLoaded.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
+import Loading from './layouts/Loading';
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth))
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  return children;
+}
+
+export default AuthIsLoaded;
