8a14999df41847f78551bd6f83b4163c887a4baf
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-08-24 00:35:55 +0000

    chore; static types

---
+++ src/components/store/types.js
@@ -0,0 +1,5 @@
+export const SEND_TWEET = 'SEND_TWEET';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
