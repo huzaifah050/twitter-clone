158c31da29c012796aebba3df829ac681f60f39c
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-08-24 00:29:53 +0000

    feat: set up routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import MainDisplay from './components/layouts/MainDisplay';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { Switch, Route } from 'react-router-dom';
+import WelcomePage from './components/layouts/WelcomePage';
+import LoginPage from './components/layouts/auth/LoginPage';
 
 function App() {
   return (
     <>
       <ThemeProvider>
         <CSSReset />
-        <MainDisplay />
+        <Switch>
+          <Route exact path="/" component={MainDisplay} />
+          <Route path="/welcome" component={WelcomePage} />
+          <Route path="/login" component={LoginPage} />
+        </Switch>
       </ThemeProvider>
     </>
   );
