7a0c36c861a835fd07449bad3543475e968f1d52
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-04 21:31:49 +0000

    chore: integrated formik

--- src/components/layouts/AddTweet.js
+++ src/components/layouts/AddTweet.js
@@ -1,55 +1,141 @@
-import React, { useState } from 'react';
+import React from 'react';
 import img from '../../imgs/img.jpg';
 import { connect } from 'react-redux';
 import { addTweet } from '../store/actions/projectActions';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function AddTweet(props) {
-  const [tweet, setTweet] = useState('');
-  const handleChange = (e) => {
-    setTweet(e.target.value);
+function AddTweet({ picture, userImg, ...props }) {
+  const initialValues = {
+    tweet: '',
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(tweet);
-    props.addTweet(tweet);
+  const validationScheme = Yup.object({
+    tweet: Yup.string().required().min(1),
+  });
+
+  const onsubmit = (values, onSubmitProps) => {
+    console.log('Form data', values);
+    props.addTweet(values.tweet);
+    onSubmitProps.setSubmitting(false);
+    onSubmitProps.resetForm();
   };
 
-  // console.log(props);
-  return (
-    <div className="user-form-profile">
-      <div className="user-form-profile-details">
-        <div className="user-img">
-          <img src={img} className="img" alt="" />
-        </div>
+  if (picture) {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationScheme}
+        onSubmit={onsubmit}
+        validateOnMount={true}
+      >
+        {(formik) => {
+          return (
+            <div className="user-form-profile">
+              <div className="user-form-profile-details">
+                <div className="user-img">
+                  <img src={picture.img} className="img" alt="" />
+                </div>
 
-        <div className="form-details">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="tweet"
-              placeholder="What's happening?"
-              value={tweet}
-              onChange={handleChange}
-            />
-            <div className="down-controls">
-              <div className="inserts">
-                <span className="material-icons insert-icons">
-                  insert_emoticon
-                </span>
-                <span className="material-icons insert-icons">gif</span>
-                <span className="material-icons insert-icons">perm_media</span>
-              </div>
-              <div className="count-tweet">
-                <div className="count-cirle ccol"></div>
-                <button className="tweet-btn ccol">Tweet</button>
+                <div className="form-details">
+                  <Form>
+                    <Field
+                      type="text"
+                      name="tweet"
+                      placeholder="What's happening?"
+                      autoComplete="off"
+                    />
+
+                    <div className="down-controls">
+                      <div className="inserts">
+                        <span className="material-icons insert-icons">
+                          insert_emoticon
+                        </span>
+                        <span className="material-icons insert-icons">gif</span>
+                        <span className="material-icons insert-icons">
+                          perm_media
+                        </span>
+                      </div>
+                      <div className="count-tweet">
+                        <div className="count-cirle ccol"></div>
+                        <button
+                          type="submit"
+                          disabled={
+                            !(formik.dirty && formik.isValid) ||
+                            formik.isSubmitting
+                          }
+                          className="tweet-btn ccol"
+                        >
+                          Tweet
+                        </button>
+                      </div>
+                    </div>
+                  </Form>
+                </div>
               </div>
             </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+          );
+        }}
+      </Formik>
+    );
+  } else {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationScheme}
+        onSubmit={onsubmit}
+        validateOnMount={true}
+      >
+        {(formik) => {
+          return (
+            <div className="user-form-profile">
+              <div className="user-form-profile-details">
+                <div className="user-img">
+                  <img src={userImg} className="img" alt="" />
+                </div>
+
+                <div className="form-details">
+                  <Form>
+                    <Field
+                      type="text"
+                      name="tweet"
+                      placeholder="What's happening?"
+                      autoComplete="off"
+                    />
+
+                    <div className="down-controls">
+                      <div className="inserts">
+                        <span className="material-icons insert-icons">
+                          insert_emoticon
+                        </span>
+                        <span className="material-icons insert-icons">gif</span>
+                        <span className="material-icons insert-icons">
+                          perm_media
+                        </span>
+                      </div>
+                      <div className="count-tweet">
+                        <div className="count-cirle ccol"></div>
+                        <button
+                          type="submit"
+                          disabled={
+                            !(formik.dirty && formik.isValid) ||
+                            formik.isSubmitting
+                          }
+                          className="tweet-btn ccol"
+                        >
+                          Tweet
+                        </button>
+                      </div>
+                    </div>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </Formik>
+    );
+  }
 }
 
 const mapDispatchToProps = {
