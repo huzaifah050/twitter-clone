1142d4c10ead1fff30e1b297a459e4fee0fc37ee
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-04 21:42:55 +0000

    chore: Tracking authentication

--- src/components/store/reducers/authReducer.js
+++ src/components/store/reducers/authReducer.js
@@ -7,28 +7,42 @@ import {
   LOGOUT_FAILURE,
 } from '../types';
 
-const initialState = {};
+const initialState = {
+  isLogged: false,
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case LOGIN_SUCCESS:
       console.log('Login success');
-      return state;
+      return {
+        ...state,
+        isLogged: true,
+      };
     case LOGIN_FAILURE:
       console.log('log in fail');
       return state;
 
     case SIGN_UP_SUCCESS:
       console.log('registered');
+      return {
+        ...state,
+        isLogged: true,
+      };
+    case 'TEST':
+      console.log('testing');
+      console.log(state);
       return state;
-
     case SIGN_UP_FAILURE:
       console.log(payload);
       return state;
 
     case LOGOUT_SUCCESS:
       console.log('logout success');
-      return state;
+      return {
+        ...state,
+        isLogged: false,
+      };
 
     case LOGOUT_FAILURE:
       console.log('logout fail', payload);
