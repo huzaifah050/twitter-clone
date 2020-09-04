db67418cc34773827c86d193ab85fc79d9cadcad
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-04 21:32:31 +0000

    chore: Routing feature to links

--- src/components/layouts/ControlPanel.js
+++ src/components/layouts/ControlPanel.js
@@ -2,18 +2,23 @@ import React from 'react';
 import BigTweetBtn from './BigTweetBtn';
 import IconLink from './IconLink';
 import ControlProfile from './ControlProfile';
+import { Link } from 'react-router-dom';
 
 function ControlPanel() {
   return (
     <div className="control-panel-content">
       <div className="icons-section">
         <div className="icons-container">
-          <IconLink link={'Home'} iconn={'home'} />
+          <Link to="/">
+            <IconLink link={'Home'} iconn={'home'} />
+          </Link>
           <IconLink link={'Notifications'} iconn={'notification_important'} />
           <IconLink link={'Messages'} iconn={'email'} />
           <IconLink link={'Bookmarks'} iconn={'turned_in_not'} />
           <IconLink link={'List'} iconn={'subject'} />
-          <IconLink link={'Profile'} iconn={'account_circle'} />
+          <Link to="/profile">
+            <IconLink link={'Profile'} iconn={'account_circle'} />
+          </Link>
           <IconLink link={'More'} iconn={'more_horiz'} />
 
           <BigTweetBtn />
