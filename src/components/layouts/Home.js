a873369e937c06bcde0519778497a825e21a9abe
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-04 21:33:54 +0000

    feat: fetching data from db and outputing them

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -1,30 +1,87 @@
 import React from 'react';
 import AddTweet from './AddTweet';
 import SIngleTweet from './SIngleTweet';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Loading from './Loading';
+import { useSelector } from 'react-redux';
+import userImg from '../../imgs/user.jpg';
 
-function Home() {
-  return (
-    <div className="home-section col">
-      <div className="home-content">
-        <div className="top-home-bar">
-          <h2>Home</h2>
-        </div>
+function Home({ tweets, pictures }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  const picture = pictures
+    ? pictures.find((picture) => picture.id === auth.uid)
+    : null;
+  if (picture) {
+    return (
+      <div className="home-section col">
+        <div className="home-content">
+          <div className="top-home-bar">
+            <h2>Home</h2>
+          </div>
 
-        <AddTweet />
+          <AddTweet picture={picture} userImg={userImg} />
 
-        <div className="all-tweets">
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
-          <SIngleTweet />
+          <div className="all-tweets">
+            {tweets ? (
+              tweets.map((tweet) => {
+                return (
+                  <SIngleTweet
+                    picture={picture}
+                    key={tweet.id}
+                    userImg={userImg}
+                    tweet={tweet}
+                  />
+                );
+              })
+            ) : (
+              <Loading />
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="home-section col">
+        <div className="home-content">
+          <div className="top-home-bar">
+            <h2>Home</h2>
+          </div>
+
+          <AddTweet picture={picture} userImg={userImg} />
+
+          <div className="all-tweets">
+            {tweets ? (
+              tweets.map((tweet) => {
+                console.log(tweet);
+                return (
+                  <SIngleTweet
+                    picture={picture}
+                    key={tweet.id}
+                    userImg={userImg}
+                    tweet={tweet}
+                  />
+                );
+              })
+            ) : (
+              <Loading />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  tweets: state.firestore.ordered.tweets,
+  pictures: state.firestore.ordered.pictures,
+});
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'tweets' }]),
+  firestoreConnect([{ collection: 'pictures' }])
+)(Home);
