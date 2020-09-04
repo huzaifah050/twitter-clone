d43c4523de2158c4e95134b2bf5d5ce42215b8d5
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-04 21:40:22 +0000

    chore: add tweet reducer

--- src/components/store/reducers/tweeterReducer.js
+++ src/components/store/reducers/tweeterReducer.js
@@ -1,10 +1,18 @@
+import { ADD_TODO, ADD_TODO_FAILURE, IMAGE_UPLOADED } from '../types';
+
 const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    // case typeName:
-    //   return { ...state, ...payload }
+    case ADD_TODO:
+      return { ...state, ...payload };
 
+    case ADD_TODO_FAILURE:
+      console.log(payload);
+      return state;
+    case IMAGE_UPLOADED:
+      console.log('image uploaded');
+      return state;
     default:
       return state;
   }
