160ed9ba9581ac59c16c7f08ef35cb74311a691f
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-06 23:43:38 +0000

    feat: making user return to previous route

--- src/components/layouts/SIngleTweet.js
+++ src/components/layouts/SIngleTweet.js
@@ -1,51 +1,74 @@
 import React from 'react';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { whichRoute, homeRoute } from '../store/actions/projectActions';
+import userImg from '../../imgs/user.jpg';
 
-function SIngleTweet({ tweet, picture, userImg }) {
+function SIngleTweet({ tweet, pictures }) {
+  // const atHome = useSelector((state) => state.tweeter.atHome);
+  // console.log(atHome);
+  let location = useLocation();
+  // console.log(location.pathname);
+  const dispatch = useDispatch();
+  if (location.pathname === '/') {
+    // console.log('from home');
+    dispatch(homeRoute());
+  } else if (location.pathname === '/profile') {
+    // console.log('from profile');
+    dispatch(whichRoute());
+  }
+
+  let picture = pictures
+    ? pictures.find((picture) => picture.id === tweet.p_id)
+    : null;
+
   return (
-    <div className="tweets-display">
-      <div className="tweet-container">
-        <div className="tweet-content">
-          <div className="img-section">
-            <img src={tweet.photo || userImg} className="imgg" alt="" />
-            {/* {tweet.photo ? (
-                <img src={tweet.photo} className="imgg" alt="" />
+    <Link to={`/tweet/${tweet.id}`}>
+      <div className="tweets-display">
+        <div className="tweet-container">
+          <div className="tweet-content">
+            <div className="img-section">
+              {picture ? (
+                <img src={picture.img} className="imgg" alt="" />
               ) : (
                 <img src={userImg} className="imgg" alt="" />
-              )} */}
-          </div>
-          <div className="single-tweet">
-            <div className="user-handle-time">
-              <p>
-                <span className="user-name">{tweet.name}</span>
-                <span className="user-handle">{`@${tweet.handle}`}</span>
-                <span className="bull">&bull;</span>
-                <span className="time">
-                  {moment(tweet.date.toDate().toString()).fromNow()}
-                </span>
-              </p>
+              )}
             </div>
-            <p>{tweet.tweet}</p>
-            <div className="tweet-controls-icons">
-              <div className="interactions-container">
-                <i className="far fa-comment tweet-icons"></i>
+            <div className="single-tweet">
+              <div className="user-handle-time">
+                <p>
+                  <span className="user-name">{tweet.name}</span>
+                  <span className="user-handle">{`@${tweet.handle}`}</span>
+                  <span className="bull">&bull;</span>
+                  <span className="time">
+                    {moment(tweet.date.toDate().toString()).fromNow()}
+                  </span>
+                </p>
               </div>
-              <div className="interactions-container">
-                <i className="fas fa-retweet tweet-icons"></i>
-                <p className="interaction-count">{tweet.numRetweeted}</p>
-              </div>
-              <div className="interactions-container">
-                <i className="fas fa-heart tweet-icons"></i>
-                <p className="interaction-count">{tweet.numLiked}</p>
-              </div>
-              <div className="interactions-container">
-                <i className="fas fa-trash tweet-icons"></i>
+              <p>{tweet.tweet}</p>
+              <div className="tweet-controls-icons">
+                <div className="interactions-container">
+                  <i className="far fa-comment tweet-icons"></i>
+                </div>
+                <div className="interactions-container">
+                  <i className="fas fa-retweet tweet-icons"></i>
+                  <p className="interaction-count">{tweet.numRetweeted}</p>
+                </div>
+                <div className="interactions-container">
+                  <i className="fas fa-heart tweet-icons"></i>
+                  <p className="interaction-count">{tweet.numLiked}</p>
+                </div>
+                <div className="interactions-container">
+                  <i className="fas fa-trash tweet-icons"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
