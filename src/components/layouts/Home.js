c37098ac405ed3dda5a7c1fd1ece8e65540f47e7
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-09-06 23:41:09 +0000

    chore: refactoring

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -13,66 +13,35 @@ function Home({ tweets, pictures }) {
   const picture = pictures
     ? pictures.find((picture) => picture.id === auth.uid)
     : null;
-  if (picture) {
-    return (
-      <div className="home-section col">
-        <div className="home-content">
-          <div className="top-home-bar">
-            <h2>Home</h2>
-          </div>
 
-          <AddTweet picture={picture} userImg={userImg} />
+  return (
+    <div className="home-section col">
+      <div className="home-content">
+        <div className="top-home-bar">
+          <h2>Home</h2>
+        </div>
 
-          <div className="all-tweets">
-            {tweets ? (
-              tweets.map((tweet) => {
-                return (
-                  <SIngleTweet
-                    picture={picture}
-                    key={tweet.id}
-                    userImg={userImg}
-                    tweet={tweet}
-                  />
-                );
-              })
-            ) : (
-              <Loading />
-            )}
-          </div>
+        <AddTweet picture={picture} userImg={userImg} />
+
+        <div className="all-tweets">
+          {tweets ? (
+            tweets.map((tweet) => {
+              return (
+                <SIngleTweet
+                  pictures={pictures}
+                  key={tweet.id}
+                  userImg={userImg}
+                  tweet={tweet}
+                />
+              );
+            })
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="home-section col">
-        <div className="home-content">
-          <div className="top-home-bar">
-            <h2>Home</h2>
-          </div>
-
-          <AddTweet picture={picture} userImg={userImg} />
-
-          <div className="all-tweets">
-            {tweets ? (
-              tweets.map((tweet) => {
-                console.log(tweet);
-                return (
-                  <SIngleTweet
-                    picture={picture}
-                    key={tweet.id}
-                    userImg={userImg}
-                    tweet={tweet}
-                  />
-                );
-              })
-            ) : (
-              <Loading />
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
